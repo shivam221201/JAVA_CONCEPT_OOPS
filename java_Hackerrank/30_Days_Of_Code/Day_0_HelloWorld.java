a7207a30fbeefab8fdc74c3609681f50fac46a68
shivam221201 <500061432@stu.upes.ac.in>
2020-07-20 19:21:10 +0530

    Java data types practice Hackerrank

--- java_Hackerrank/30_Days_Of_Code/Day_0_HelloWorld.java
+++ java_Hackerrank/30_Days_Of_Code/Day_0_HelloWorld.java
@@ -5,19 +5,15 @@ import java.math.*;
 import java.util.regex.*;
 public class Day_0_HelloWorld {
 	public static void main(String[] args) {
-        // Create a Scanner object to read input from stdin.
+       
 		Scanner scan = new Scanner(System.in); 
-		
-		// Read a full line of input from stdin and save it to our variable, inputString.
+			
 		String inputString = scan.nextLine(); 
-
-		// Close the scanner object, because we've finished reading 
-        // all of the input from stdin needed for this challenge.
+		
 		scan.close(); 
       
-		// Print a string literal saying "Hello, World." to stdout.
 		System.out.println("Hello, World.");
-        System.out.println("/n" + inputString );
+        System.out.println("" + inputString );
       
 	
 	}
---
+++ java_Hackerrank/30_Days_Of_Code/solution.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+
+public class solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+
+        int a= scan.nextInt();
+
+        double b = scan.nextDouble();
+         
+        scan.nextLine();
+        String str = scan.nextLine();
+
+        int sum = i+a;
+        
+        double sum2 = d+b;
+       
+        
+        System.out.println("" + sum);
+        System.out.println("" + sum2);
+        System.out.println(s + "" + str);
+
+        
+        scan.close();
+    }
+}
